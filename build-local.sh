af081eebe76cb44910de9e96ee1bad4e6b49f40d
leon <leon@localhost>
2023-06-15 21:45:39 +0200

    Script for build and local run added

---
+++ build-local.sh
@@ -0,0 +1,4 @@
+docker run -it --rm --name maven-taxes -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.2-amazoncorretto-17-debian mvn clean install \
+&& cp target/taxes-0.0.1-SNAPSHOT.war docker/taxes.war \
+&& docker image build -t codechallenge/taxes:latest docker/ \
+&& docker run -it -p 8888:8080 --rm codechallenge/taxes:latest
