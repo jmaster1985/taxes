10aeb218c30f09917258e51533f7b39bb5557829
leon <leon@localhost>
2023-06-15 21:26:54 +0200

    Initial unit test added

--- src/test/java/com/codechallenge/taxes/TaxesApplicationTests.java
+++ src/test/java/com/codechallenge/taxes/TaxesApplicationTests.java
@@ -3,12 +3,19 @@ package com.codechallenge.taxes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TaxesApplicationTests {
 
 	@Test
 	void contextLoads() {
 
+	}
+
+	@Test
+	void isUnitTestingActive() {
+		assertEquals(1, 1);
 	}
 
 }
