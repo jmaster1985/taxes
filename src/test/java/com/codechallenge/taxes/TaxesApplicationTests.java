8ac1a7aea494e2d73eae433b18862644f873a859
leon <leon@localhost>
2023-06-15 21:18:17 +0200

    Spring boot version 3.1.0 added

---
+++ src/test/java/com/codechallenge/taxes/TaxesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codechallenge.taxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
