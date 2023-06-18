508d22fce515ae54534adbd10471837d1d2c9fbd
leon <leon@localhost>
2023-06-18 14:53:45 +0200

    Use-case implemented 'GetTaxClassesUseCase'
    new use-case GetTaxClassesUseCase
    tests added in GetTaxClassesUseCaseTests

---
+++ src/test/java/com/codechallenge/taxes/usecase/GetTaxClassesUseCaseTests.java
@@ -0,0 +1,26 @@
+package com.codechallenge.taxes.usecase;
+
+import com.codechallenge.taxes.model.TaxClassCollection;
+import com.codechallenge.taxes.usecase.exception.UseCaseRunFailedException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+public class GetTaxClassesUseCaseTests {
+
+    private final GetTaxClassesUseCase getTaxClassesUseCase;
+
+    @Autowired
+    public GetTaxClassesUseCaseTests(GetTaxClassesUseCase getTaxClassesUseCase) {
+        this.getTaxClassesUseCase = getTaxClassesUseCase;
+    }
+
+    @Test
+    public void isTaxClassCollectionLoadedTest() throws UseCaseRunFailedException {
+        TaxClassCollection taxClassCollection = getTaxClassesUseCase.run();
+        assertFalse(taxClassCollection.getTaxClassList().isEmpty());
+    }
+}
